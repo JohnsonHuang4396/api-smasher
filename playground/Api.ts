3e1fdbf515b40aefefe16ea75a64872083c5168f
JohnsonHuang4396 <83126532+JohnsonHuang4396@users.noreply.github.com>
2025-03-27 22:57:52 +0800

    chore: playground

--- playground/Api.ts
+++ playground/Api.ts
@@ -1,8 +1,23 @@
-// @ts-nocheck
 export async function getWechatAssessmentByEmployee(body: WechatAssessmentQO) {
   const result = await axios.post<RIPageWechatEmployeeConversationVO>(
-    '/api/wechatContentMsg/getWechatAssessmentByEmployee',
+    "/api/wechatContentMsg/getWechatAssessmentByEmployee",
     body
-  )
-  return result.data.data
+  );
+  return result.data.data;
+}
+
+export async function getWechatAssessmentByEmployee(body: WechatAssessmentQO) {
+  const result = await axios.post<RIPageWechatEmployeeConversationVO>(
+    "/api/wechatContentMsg/getWechatAssessmentByEmployee",
+    body
+  );
+  return result.data.data;
+}
+
+export async function getWechatAssessmentByEmployee(body: WechatAssessmentQO) {
+  const result = await axios.post<RIPageWechatEmployeeConversationVO>(
+    "/api/wechatContentMsg/getWechatAssessmentByEmployee",
+    body
+  );
+  return result.data.data;
 }
